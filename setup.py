2d6efb8c0d23af9eba0b2c7ead5522ee7c19e207
Gibson <Jeremy.Gibson@ncdcr.gov>
2018-01-19 15:21:06 -0500

    Second Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ThreadedInventory',
+    version='0.0.1',
+    packages=['classes'],
+    url='',
+    license='LGPL-3.0',
+    author='Jeremy Gibson',
+    author_email='jeremy.gibson@ncdcr.gov',
+    description='A tool to quickly inventory, based on identity not fixity, a digital repository'
+)
